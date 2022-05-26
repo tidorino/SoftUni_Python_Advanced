009034198dbd7076f1a9b28ea8499a12d1b865d4
tidorino <tidorino@hotmail.com>
2022-05-26 10:57:30 +0300

    exercise - sets

---
+++ tuples_and_sets_exercise/03_periodic_table.py
@@ -0,0 +1,22 @@
+"""
+On the first line you will be given a number n - the count of input lines
+that you are going to receive. On the next n lines, you will be receiving chemical compounds,
+separated by a single space.Your task is to print all the unique ones.
+3
+Ge Ch O Ne
+Nb Mo Tc
+O Ne
+
+"""
+
+n = int(input())
+
+chemical_elements = set()
+for _ in range(n):
+    item = input().split()
+
+    for i in item:
+        chemical_elements.add(i)
+
+[print(element) for element in chemical_elements]
+
