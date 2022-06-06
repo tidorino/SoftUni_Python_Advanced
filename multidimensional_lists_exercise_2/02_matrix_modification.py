ac974581121b55409b6439386b3d733a34033637
tidorino <tidorino@hotmail.com>
2022-06-06 17:59:53 +0300

    exercises - multidimensional list

---
+++ multidimensional_lists_exercise_2/02_matrix_modification.py
@@ -0,0 +1,24 @@
+rows = int(input())
+matrix = [[int(x) for x in input().split(' ')] for _ in range(rows)]
+
+while True:
+    command = input()
+    if command == 'END':
+        for row in matrix:
+            print(*row, sep=' ')
+        break
+
+    new_command = command.split(' ')
+    row, col, value = [int(x) for x in new_command[1:]]
+
+    if row < 0 or col < 0 or row >= rows - 1 or col >= rows - 1:
+        print(f'Invalid coordinates')
+        continue
+
+    if new_command[0] == 'Add':
+        matrix[row][col] += value
+
+    else:
+        matrix[row][col] -= value
+
+
