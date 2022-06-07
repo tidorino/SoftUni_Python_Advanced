9e6884628e54d124529614caeb20d44e7eb611f3
tidorino <tidorino@hotmail.com>
2022-06-07 18:10:16 +0300

    multidimensional list exercise - add text of problem

--- multidimensional_lists_exercise_2/03_knight_game.py
+++ multidimensional_lists_exercise_2/03_knight_game.py
@@ -1,3 +1,22 @@
+"""
+The knight game is played on a board with dimensions N x N.
+You will receive a board with "K" for knights and "0" for empty cells.
+Your task is to remove knights until no knights that can attack one another with one move are left.
+Input
+•	On the first line, you will receive integer N - the size of the board
+•	On the following N lines, you will receive strings with "K" and "0"
+Output
+•	Print a single integer with the minimum number of knights that need to be removed
+************
+5
+0K0K0
+K000K
+00K00
+K000K
+0K0K0
+
+"""
+
 def find_count(board, row, column):
     moves = [
         [row - 2, column - 1],
