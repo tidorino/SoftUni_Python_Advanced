fef8d1f7c146f8ef3bab667d33f3911f3c1980e0
tidorino <tidorino@hotmail.com>
2022-06-11 21:52:14 +0300

    file handling exercises

---
+++ file_handling_lab/word_count_files/05_word_count.py
@@ -0,0 +1,26 @@
+import re
+
+
+def read_words():
+    with open('../word_count_files/words.txt', 'r') as file:
+        return file.read().split(' ')
+
+
+def count_words_in_file(words):
+    words_counts = {
+        word:0 for word in words
+    }
+    with open('../word_count_files/input.txt', 'r') as file:
+        for line in file:
+            word_in_line = [w.lower() for w in re.findall(r'\b\S+\b', line)]
+            for word in words:
+                words_counts[word] += word_in_line.count(word)
+
+    return words_counts
+
+
+words_counts = count_words_in_file(read_words())
+sorted_words_count = sorted(words_counts.items(), key=lambda x: x[1], reverse=True)
+[
+    print(f'{w} - {c}') for w, c in sorted_words_count
+]
