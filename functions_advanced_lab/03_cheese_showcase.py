cd8c257536bb7730300e0ddc2ba87dd794c6f414
tidorino <tidorino@hotmail.com>
2022-06-14 15:27:13 +0300

    function advanced exercises

---
+++ functions_advanced_lab/03_cheese_showcase.py
@@ -0,0 +1,22 @@
+def sorting_cheeses(**kwargs):
+    sorted_cheeses = sorted(
+        kwargs.items(),
+        key=lambda x: (- len(x[1]), x[0]),
+    )
+    result = []
+    for cheese_name, quantity in sorted_cheeses:
+        result.append(cheese_name)
+        result.extend(sorted(
+            quantity,
+            reverse=True
+        ))
+    return '\n'.join([str(x) for x in result])
+
+
+print(
+    sorting_cheeses(
+        Parmesan=[102, 120, 135], 
+        Camembert=[100, 100, 105, 500, 430], 
+        Mozzarella=[50, 125],
+    )
+)
